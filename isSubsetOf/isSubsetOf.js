05b710276f7f3b74ffd985af8df8d9424fdde69b
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-10-20 12:25:06 -0400

    finished susbset and norepeat

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -22,6 +22,53 @@
  * including non-strings.
 */
 
+//Problem: check whether an array is included in the input array.
+
+//Variables: context array, input array
+
+//Constraints: 
+  //both arrays will only contain strings,
+  //disregard duplicates
+
+//Overview
+  //compare elements in subset array to input array
+  //if all elements in subset array are present at all in input array return true
+  //if not, return false
+
+//pseudo-code
+
+//declare a flag to true;
+//pass subset array into a variable
+
+
+//interate over context array
+  //if flag is false
+    //break
+  //if match for current element not in array obj
+    //set flag to false
+//return flag
+//=============================================
 Array.prototype.isSubsetOf = function(array){
-  // Your code here
+  var subset = this;
+  var input = {};
+  var flag = true;
+
+  //pass input array into object for constant time access
+  for (var i = 0; i < array.length; i++){
+    //debugger;
+    input[array[i]] = array[i];
+  }
+
+  //interate over context array
+  for (var e = 0; e < subset.length; e++){
+    //if flag is false
+    if (!flag){
+      break;
+    } else if (!input.hasOwnProperty(subset[e])){
+      //set flag to false
+      flag = false;
+    }
+  }
+  //return flag
+  return flag;
 };
--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,32 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
+// INPUT: String
+// OUTPUT: A single character
+
+//Overview
+
+// 1. go through each character in the input string
+// 2. check whether a regexp returns true for more than one character
+// 3. if it doesn't return the character.
+
+//PSEUDO-CODE
+
+//declare a variable to hold an empty array
+//declare a variable to hold current character
+
+//loop through each character in the string
+  //loop through string
+    //if array length > 1
+      //return the character
+    //push any regexp matches to array
+
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  //loop through each character in the string
+  for (c = 0; c < string.length; c++){
+    var re = new RegExp(string.charAt(c),"gi");
+    if (string.match(re).length === 1){
+      return string.charAt(c);
+    }
+  }
 };
