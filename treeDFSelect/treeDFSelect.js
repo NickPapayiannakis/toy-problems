0e41a361c7b000aecb7df1f0bee303b3e6f0120e
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-10-23 12:24:05 -0400

    Attempted treeDFSelect.

--- treeDFSelect/treeDFSelect.js
+++ treeDFSelect/treeDFSelect.js
@@ -7,16 +7,16 @@
   * for which the filter returns true.
   *
   * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   root1.DFSelect(function (value, depth) {
-  *     return value % 2;
-  *   })
+    var root1 = new Tree(1);
+    var branch2 = root1.addChild(2);
+    var branch3 = root1.addChild(3);
+    var leaf4 = branch2.addChild(4);
+   var leaf5 = branch2.addChild(5);
+    var leaf6 = branch3.addChild(6);
+    var leaf7 = branch3.addChild(7);
+    root1.DFSelect(function (value, depth) {
+      return value % 2;
+    })
   *   // [1, 5, 3, 7]
   *
   *   root1.DFSelect(function (value, depth) {
@@ -35,7 +35,24 @@ var Tree = function(value){
   this.children = [];
 };
 
-Tree.prototype.DFSelect = function(filter) {
+Tree.prototype.DFSelect = function(filter, node) {
+  var results = [];
+
+  // Base case
+    // test returns true
+    if (filter(node)){
+      results.push(node.value);
+    }
+
+  // Recursive
+    // iterate over children
+    for(var c = 0; c < node.children; c++){
+      //call function on all children
+      filter(node[c]);
+    }
+
+  return results;
+
 };
 
 
