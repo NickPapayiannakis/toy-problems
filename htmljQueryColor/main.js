057b724d2f67504b2acd1985db8aef7a90abc3d5
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-10-27 12:28:06 -0400

    Removed empty spaces

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -1,4 +1,9 @@
-$(function(){
+/*
+select the inner html of p tags
+use regexp to match words inside of p tags
+wrap each matched word in span tag
+*/
+
   // --------------STEP 1--------------
   // wrap every word in every `<p>` tag with a `<span>` tag.
   // for example: <p>Hey there</p>
@@ -12,5 +17,13 @@ $(function(){
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+
+$(function(){
+  var spanify = function (match) {
+    return '<span>' + match + '</span>';
+  }
+
+  var x = $('p').html().replace(/(\w+)/g, spanify);
+  $('p').empty().append(x);
 
 });
